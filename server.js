aef4307a228414908ae0dac131070f1198de3e0a
Mohammed Safiq Nishar S <safiqatwork@gmail.com>
2026-06-18 11:43:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const networkRoutes = require("./routes/networkRoutes");
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 //Test Route
 app.get("/", (req, res) => {
